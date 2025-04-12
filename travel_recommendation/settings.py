1eacd9fccbefdfc1b14863479ce22931e818f253
musleh <khan.muslehuddin0001@gmail.com>
2025-04-12 20:49:11 +0600

    # Feature - adds swagger for api documentations

--- travel_recommendation/settings.py
+++ travel_recommendation/settings.py
@@ -25,6 +25,7 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     # external app
+    "drf_yasg",
     "rest_framework",
     "rest_framework_simplejwt",
     "rest_framework_simplejwt.token_blacklist",
--- travel_recommendation/urls.py
+++ travel_recommendation/urls.py
@@ -1,9 +1,32 @@
+from drf_yasg import openapi
 from django.contrib import admin
 from django.urls import path, include
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
 
-# fmt: off
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="Travel Recommendation System",
+        default_version="v1",
+        description="Test description",
+        contact=openapi.Contact(email="khan.muslehuddin0001@gmail.com"),
+    ),
+    public=True,
+    permission_classes=(permissions.AllowAny,),
+)
+
 urlpatterns = [
-    path("admin/", admin.site.urls), 
+    path(
+        "swagger.<format>/", schema_view.without_ui(cache_timeout=0), name="schema-json"
+    ),
+    path(
+        "swagger/",
+        schema_view.with_ui("swagger", cache_timeout=0),
+        name="schema-swagger-ui",
+    ),
+    path("redoc/", schema_view.with_ui("redoc", cache_timeout=0), name="schema-redoc"),
+    path("admin/", admin.site.urls),
     path("api/v1/", include("travel.urls")),
     path("api/v1/auth/", include("accounts.urls")),
 ]
