13363773c26def24a737574a3f0291d750e022a4
musleh <khan.muslehuddin0001@gmail.com>
2025-04-12 14:05:17 +0600

    # Feature - write script to load districts data

--- travel/management/commands/load_districts_data.py
+++ travel/management/commands/load_districts_data.py
@@ -1,8 +1,44 @@
+import os
 import requests
 from django.core.management.base import BaseCommand
+
+from travel.models import District
 
 
 class Command(BaseCommand):
     help = "Load districts data into db"
 
-    def handle(self, *args, **kwargs): ...
+    def handle(self, *args, **kwargs):
+        if districts_data := self.fetch_district_data():
+            for district in districts_data.get("districts"):
+                if District.objects.filter(name=district.get("name")).exists():
+                    continue
+
+                District.objects.create(
+                    division_id=district.get("division_id"),
+                    name=district.get("name"),
+                    bn_name=district.get("bn_name"),
+                    lat=float(district.get("lat")),
+                    long=float(district.get("long")),
+                )
+
+            self.stdout.write(self.style.SUCCESS("Successfully synced districts data."))
+        else:
+            self.stdout.write(self.style.WARNING("No district data found to sync."))
+
+    def fetch_district_data(self):
+        result = dict()
+
+        try:
+            response = requests.get(os.getenv("DISTRICT_INFO_URL"))
+            result = response.json()
+        except requests.RequestException as error:
+            self.stderr.write(
+                self.style.ERROR(f"Failed to fetch district data: {error}")
+            )
+        except Exception as error:
+            self.stderr.write(
+                self.style.ERROR(f"Failed to fetch district data: {error}")
+            )
+
+        return result
--- travel/models.py
+++ travel/models.py
@@ -7,6 +7,8 @@ class District(models.Model):
     bn_name = models.CharField(max_length=255)
     lat = models.DecimalField(max_digits=10, decimal_places=7)
     long = models.DecimalField(max_digits=10, decimal_places=7)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
 
     class Meta:
         db_table = "districts"
